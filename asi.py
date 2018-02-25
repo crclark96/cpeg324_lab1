f468f97d8b376b6b2ec628977b339fb8546279e7
zirons1 <zirons@udel.edu>
2018-02-25 16:08:05 -0500

    Moved eightbit and dec_convert into asi.py

--- asi.py
+++ asi.py
@@ -9,8 +9,82 @@ ASI ISA
 """
 
 import sys
-from eightbit import eightbit
-from dec_convert import dec_convert
+
+def dec_convert(b):
+    res = 0
+
+    if b[:1] == '1':        
+        for i in range(1,4):
+            if b[i] == '0':
+                res -= 2**(3-i)
+        res -= 1
+
+    else:
+        for i in range(1,4):
+            if b[i] == '1':
+                res += 2**(3-i)
+
+    return res
+
+
+def eightbit(i):
+
+    ans = ''
+
+    if i > 0:
+        n = 1
+
+        while n < i:          
+            n *= 2
+
+        while n != 0:
+            if (i - n < 0):
+                ans += '0'
+                
+            else:
+                i -= n                
+                ans += '1'                
+            n = int(n/2)
+
+        ans = '0'*(8- len(ans)) + ans
+        ans = '0b' + ans[-8:]
+                
+        return(ans)
+            
+    elif i < 0:
+        i *= -1
+        n = 1
+
+        while n < i:          
+            n *= 2
+
+        while n != 0:
+            if (i - n < 0):
+                ans += '1'
+                
+            else:
+                i -= n                
+                ans += '0'                
+            n = int(n/2)
+
+        if ans[-1:] == '0':
+            ans = ans[:-1] + '1'
+
+        else:
+            x = -1
+
+            while ans[x] == '1' and x < 8:
+                x -= 1
+            ans = ans[:len(ans) - abs(x)] + '1' + '0'*(abs(x) - 1)
+
+        ans = '1'*(8- len(ans)) + ans
+        ans = '0b' + ans[-8:]
+
+        return(ans)
+
+    else:
+        return('0b00000000')
+
 
 def operate(registers, instruction):
 
